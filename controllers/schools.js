76f6fdac82a79fcdea66781d79993a53d7a7a95e
kalmas <kyle.almas@dominionenterprises.com>
2013-10-24 17:17:37 -0400

    added methods to schools controller and cleaned up some

--- controllers/schools.js
+++ controllers/schools.js
@@ -1,18 +1,32 @@
 var mongoose = require('mongoose')
-  , School = mongoose.model('School')
-  , _ = require('underscore');
+  , School = mongoose.model('School');
 
 module.exports = {
-	all: function(req, res) {
-		School.find().exec(function(err, ret) {
-			if(err) {
-     	 		return res.send(500, err);
-    		}
-
-    		return res.json(ret);
+  school: function(req, res, next) {
+    School.findOne({ _id: req.params.schoolId }, function(err, school) {
+      if(err) {
+        res.send(500, err);
+      } else if(!school) {
+        res.send(404);
+      } else {
+        res.json(school);
+      }
+      return next();
+    });
+  },
+  
+  all: function(req, res, next) {
+    School.find().exec(function(err, schools) {
+        if(err) {
+          res.send(500, err);
+        } else {
+          res.json(schools);
+        }
+        return next();
 		});
-	},
-	near: function(req, res) {
+  },
+  
+	near: function(req, res, next) {
 		var METERS_IN_MILE = 1609.34
 			, MILES_IN_METER = 0.000621371
 			, longitude = parseFloat(req.params.lon)
@@ -20,20 +34,26 @@ module.exports = {
 			, maxDistance = (typeof req.params.maxDistance === 'undefined') ? 
 				15 : parseFloat(req.params.maxDistance);
 		School.aggregate( [{
-        $geoNear: {
-          near: {	type : "Point" 
-            , coordinates: [ longitude, latitude ] }
-          , distanceField: "dist.calculated"
-          , spherical: true
-          , maxDistance: METERS_IN_MILE * maxDistance
-          , distanceMultiplier : MILES_IN_METER
-        }
-      }], function(err, docs) {
-         if(err) return res.send(500, err);
-         return res.json(docs);
-      });
+		  $geoNear: {
+		    near: {	type : "Point" 
+		      , coordinates: [ longitude, latitude ] }
+	      , distanceField: "calculated.dist"
+	      , spherical: true
+        , maxDistance: METERS_IN_MILE * maxDistance
+        , distanceMultiplier : MILES_IN_METER
+      }
+	  }], function(err, schools) {
+		    if(err) {
+		      res.send(500, err);
+		    } else {
+		      res.json(schools);
+		    }
+		    return next();
+		});
 	},
-	upsert: function(req, res) {
+	
+	upsert: function(req, res, next) {
+	  console.log(req.body);
 	  var schoolId = req.body._id;
 	  delete req.body._id; // findOneAndUpdate() doesn't like use to play with _id
 	  delete req.body.modified; // always set modified to Now
@@ -42,9 +62,27 @@ module.exports = {
         , req.body
         , {upsert: true}
         , function(err, school) {
-            if (err) return res.send(500, err);
-            return res.json(school);
+            if(err) {
+              res.send(500, err);
+            } else {
+              res.json(school);
+            }
+            return next();
       });
+  },
+  
+  del: function(req, res, next) {
+    var schoolId = req.params.schoolId;
+    
+    School.remove({ _id: schoolId }, function(err) {
+        if(err) {
+          res.send(500, err);
+        } else {
+          res.send(200);
+        }
+        return next();
+    });
   }
+  	
 };
 
